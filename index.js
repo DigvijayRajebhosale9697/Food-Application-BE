867b375e98a7d529a0a4b8f7b2c0429697ee0f1c
DigvijayRajebhosale <digvijay.rajebhosale@codeprotechnology.com>
2024-07-21 14:12:40 +0530

    V5

--- index.js
+++ index.js
@@ -7,20 +7,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 const FrontEndURL = process.env.FRONTENDURL;
 
-console.log('Frontend URL:', FrontEndURL);  // Add this line to log the FrontEndURL
+console.log('Frontend URL:', FrontEndURL);
 
 import connectToDatabase from './db.js';
 import createUserRouter from './Routes/CreateUser.js';
 import displayDataRouter from './Routes/DisplayData.js';
 import orderDataRouter from './Routes/OrderData.js';
 
-// Use CORS middleware
 app.use(cors({
   origin: FrontEndURL,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
 }));
+
+// Add headers for preflight requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', FrontEndURL);
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  
+  next();
+});
 
 connectToDatabase();
 
